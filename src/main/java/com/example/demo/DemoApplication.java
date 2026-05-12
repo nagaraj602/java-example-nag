ec14738f506ff565f455ca675fe275935d8c216f
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-05-12 23:07:55 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.URL;
 
 @SpringBootApplication
 @RestController
@@ -15,29 +14,26 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
-        try {
-            String publicIp = new BufferedReader(
-                    new InputStreamReader(new URL("http://checkip.amazonaws.com").openStream())
-            ).readLine();
-
-            System.out.println("\n🔥🚀 You can access the application with: http://" + publicIp + ":8085 🚀\n");
-
-        } catch (Exception e) {
-            System.out.println("⚠️🚀Unable to fetch public IP. You can access application in your browser with: http://<server_IP>:8085 🚀");
-        }
     }
 
     @GetMapping("/")
     public String home() {
-        return """
-            <html>
-                <body style="margin:0; background-color:#0a2540; display:flex; justify-content:center; align-items:center; height:100vh;">
-                    <h1 style="color:white;">
-                        Hello, Spring Boot is running on port 8085!
-                    </h1>
-                </body>
-            </html>
-            """;
+
+        String publicIp = "Unknown";
+
+        try {
+            Process process = Runtime.getRuntime().exec("curl -s ifconfig.me");
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream())
+            );
+
+            publicIp = reader.readLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "Hello, Spring Boot is running on " + publicIp + " port 8085!";
     }
 }
